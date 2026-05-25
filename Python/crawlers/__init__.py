dcd90af03d0b9e209c263779e7b2035f8702513d
kkaskkkask <2458739642@qq.com>
2026-05-25 14:45:46 +0800

    Delete Python directory

--- Python/crawlers/__init__.py
+++
@@ -1,6 +0,0 @@
-# 爬虫模块初始化文件
-from .base import CrawlerBase
-from .steam_charts import SteamChartsCrawler
-from .taptap import TapTapCrawler
-
-__all__ = ['CrawlerBase', 'SteamChartsCrawler', 'TapTapCrawler']
